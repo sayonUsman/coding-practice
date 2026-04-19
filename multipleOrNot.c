54ca2b0bd49f45d262f4c85325b9f4f18d5fedae
Sayon Usman <sayonusman.ma@gmail.com>
2026-04-19 17:30:22 +0300

    feat: solve 'Multiple or Not' problem

---
+++ multipleOrNot.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+
+    if(num1 % num2 == 0 ){
+        printf("Yes");
+    }
+    else if(num2 % num1 == 0 ){
+        printf("Yes");
+    }
+    else{
+        printf("No");
+    }
+
+    return 0;
+}
